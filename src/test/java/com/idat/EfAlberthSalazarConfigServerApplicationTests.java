623a499271589c62eb66d6cb1e8655c94b157477
usuari0 <alberthlevisalazarverde1@gmail.com>
2022-08-16 23:48:56 -0500

    first commit

---
+++ src/test/java/com/idat/EfAlberthSalazarConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfAlberthSalazarConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
